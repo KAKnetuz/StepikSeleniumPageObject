4922f855d0656cc184197c04f2b484349186a124
Екатерина <katylor@list.ru>
2023-08-18 14:38:06 +0300

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,7 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -10,12 +11,31 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert '/login/' in self.browser.current_url, 'URL страницы логина не корректный'
+        url = self.browser.current_url
+        assert "login" in url
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Нет формы авторизации'
+        assert self.is_element_present(*LoginPageLocators.LOGIN_INPUT_EMAIL), (
+            "Login input email is not presented")
+        assert self.is_element_present(*LoginPageLocators.LOGIN_INPUT_PASSWORD), (
+            "Login input password is not presented")
+        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON_LOGIN), (
+            "Login button is not presented")
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'Нет формы регистрации'
+        assert self.is_element_present(*LoginPageLocators.REGISTER_INPUT_EMAIL), (
+            "Register input email is not presented")
+        assert self.is_element_present(*LoginPageLocators.REGISTER_INPUT_PASSWORD), (
+            "Register input password is not presented")
+        assert self.is_element_present(*LoginPageLocators.REGISTER_INPUT_PASSWORD_CONFIRM), (
+            "Register input password is not presented")
+        assert self.is_element_present(*LoginPageLocators.REGISTER_BUTTON_REGISTER), (
+            "Register button is not presented")
+
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.REGISTER_INPUT_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTER_INPUT_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_INPUT_PASSWORD_CONFIRM).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON_REGISTER).click()
